7767b2c5afdf78c5cc2a80eb5399afa4006ab494
Viktor Ödman <vo222dq@student.lnu.se>
2021-12-01 14:02:03 +0100

    Adding login event for websocket

--- server/src/utils/sockets.ts
+++ server/src/utils/sockets.ts
@@ -1,6 +1,12 @@
 import { Server as ServerCreate } from 'http'; // ServerCreate är hittepå bara för att inte krocka med server
 import { Server as SocketServer, Socket } from 'socket.io';
 
+type SocketLogin = {
+    user_id: string | number;
+};
+
+export const socketConnections = {};
+
 export const initializeSocketServer = (server: ServerCreate) => {
     const io = new SocketServer(server, {
         cors: {
@@ -15,8 +21,20 @@ export const initializeSocketServer = (server: ServerCreate) => {
 
 const handleConnections = (io: SocketServer) => {
     console.log('here');
-    io.on('connection', (socket: Socket) => {
+    io.on('connect', (socket: Socket) => {
         console.log('Hurray');
         socket.emit('userConnected', { connected: true });
+
+        socket.on('login', (data: SocketLogin) => loginUser(data, socket));
     });
+};
+
+const loginUser = (loginSocketData: SocketLogin, socket: Socket) => {
+    if (loginSocketData.user_id) {
+        socketConnections[socket.id] = loginSocketData.user_id;
+    }
+
+    for (const user of Object.values(socketConnections)) {
+        console.log(user);
+    }
 };
