3fd43691062830b87fa4c45ea62607e0636c3279
Sebastian Jonsson <sj223gb@student.lnu.se>
2021-12-01 15:12:11 +0100

    #26, #31, #34 refactor

--- server/src/middlewares/userMiddleware.ts
+++ server/src/middlewares/userMiddleware.ts
@@ -1,15 +1,37 @@
-import { BaseContext } from 'koa';
+import { Context } from 'koa';
 
 export default class UserMiddleware {
 
-    public checkTestQueryParams (ctx: BaseContext, next: () => Promise<void>): void {
-        if (ctx.query.name !== 'lars') {
-            ctx.status = 400;
+    public async adminRightsCheck (ctx: Context, next: () => Promise<void>): Promise<void> {
+        if (ctx.query.role !== 'admin') {
+            ctx.status = 500;
             ctx.body = {
-                error: 'You are not lars!'
+                error: 'You are not admin!'
             };
         } else {
-            next();
+            await next();
+        }
+    }
+
+    public async moderatorRightsCheck (ctx: Context, next: () => Promise<void>): Promise<void> {
+        if (ctx.query.role !== 'moderator') {
+            ctx.status = 500;
+            ctx.body = {
+                error: 'You are not moderator!'
+            };
+        } else {
+            await next();
+        }
+    }
+
+    public async userRightsCheck (ctx: Context, next: () => Promise<void>): Promise<void> {
+        if (ctx.query.role !== 'user') {
+            ctx.status = 500;
+            ctx.body = {
+                error: 'You are not user!'
+            };
+        } else {
+            await next();
         }
     }
 }
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,28 +1,29 @@
-import { BaseContext, Context } from "koa";
-import Router, { IMiddleware } from "koa-router";
-import UserController from "../controllers/userController";
-import UserMiddleware from "../middlewares/userMiddleware";
+import { Context } from 'koa';
+import Router, { IMiddleware } from 'koa-router';
+import UserController from '../controllers/userController';
+import UserMiddleware from '../middlewares/userMiddleware';
 
 export default class UserRouter {
-  private _router: Router = new Router();
-  private controller: UserController = new UserController();
-  private middleware: UserMiddleware = new UserMiddleware();
+    private _router: Router = new Router();
+    private controller: UserController = new UserController();
+    private middleware: UserMiddleware = new UserMiddleware();
 
-  constructor() {
-    this.initializeRoutes();
-  }
+    constructor() {
+        this.initializeRoutes();
+    }
 
-  public get router(): IMiddleware<any, unknown> {
-    return this._router.routes();
-  }
+    public get router(): IMiddleware<any, unknown> {
+        return this._router.routes();
+    }
 
-  private initializeRoutes(): void {
-    this._router
-      .get("/", (ctx: Context) => this.controller.getUsers(ctx))
-      .post("/", (ctx: Context) => this.controller.addUser(ctx))
-      .allowedMethods();
-    this._router.get("/:id", (ctx: Context) =>
-      this.controller.getSingleUser(ctx)
-    );
-  }
+    private initializeRoutes(): void {
+        this._router
+            .get('/', 
+                (ctx: Context, next: () => Promise<void>) => this.middleware.adminRightsCheck(ctx, next), (ctx: Context) => this.controller.getUsers(ctx))
+            .post('/', (ctx: Context, next: () => Promise<void>) => this.middleware.adminRightsCheck(ctx, next), (ctx: Context) => this.controller.addUser(ctx))
+            .allowedMethods();
+        this._router.get('/:id', (ctx: Context) =>
+            this.controller.getSingleUser(ctx)
+        );
+    }
 }
