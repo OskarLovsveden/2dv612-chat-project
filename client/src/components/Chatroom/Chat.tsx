5dd697d0c6f49256178358f3cef1b2410bc20cac
Simon Palm <sp222ym@student.lnu.se>
2021-12-02 15:21:18 +0100

    #43 Changing styling and adding states.

--- client/src/components/Chatroom/Chat.tsx
+++ client/src/components/Chatroom/Chat.tsx
@@ -1,11 +1,8 @@
 
+import {SetStateAction, useState } from 'react'
 import Message from './Message'
 
-const messages = [
-    "hejs",
-    "halåå",
-    "tjabba"
-]
+
 const person1 = {
     name: "Erik",
     message: "Hejsan"
@@ -17,21 +14,37 @@ const person2 = {
 
 const testMess = [
     person1,
+    person2,
+    person1,
+    person2,
+    person1,
     person2
 ]
 
-const handleOnSubmit = (Event: {preventDefault: ()  => void}) => {
-    const chatMessage = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    }
-    Event.preventDefault()
-}
-export default function ChatRoom({Toggle} : {Toggle: () => void}) {
-    return (
 
-        <>
-            <div className="max-w-auto h-auto w-full m-auto bg-indigo-100 rounded p-5">
+export default function ChatRoom({Toggle} : {Toggle: () => void}) {
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+    
+    const handleName = (Event: { target: { value: SetStateAction<string> } }) => {
+        setName(Event.target.value)
+
+    }
+
+    const handleMessage = (Event: { target: { value: SetStateAction<string> } }) => {
+        setMessage(Event.target.value)
+    }
+        
+    const handleOnSubmit = (Event: {preventDefault: ()  => void}) => {
+        const chatMessage = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        }
+        Event.preventDefault()
+    }
+
+    return (
+            <div className="max-w-auto h-screen w-full m-auto bg-indigo-300 rounded p-5">
                 <header>
                     <div className="relative left-5 top-5">
                         <button onClick={Toggle} type="button" className="absolute bottom-0 right-0 bg-white rounded-md p-2 text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -43,6 +56,7 @@ export default function ChatRoom({Toggle} : {Toggle: () => void}) {
                     </div>
                 </header>
 
+                <div className="h-3/4 overflow-y-scroll">
                 <ul>
                     {testMess.map(message => (
                     <li>
@@ -50,23 +64,41 @@ export default function ChatRoom({Toggle} : {Toggle: () => void}) {
                     </li>
                     ))}
                 </ul>
+                </div>
                 <div className="mb-6 mx-4">
                     <form onSubmit={handleOnSubmit}>
-                        <input
-                        type="text"
-                        placeholder="Skicka meddelande..."
-                        className="rounded-r-lg rounded-l-lg flex-1 appearance-none border border-gray-300 py-2 px-1 lg:px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none"
-                        name="message"
-                        />
-                        <button
-                        type="submit"
-                        className="uppercase font-semibold text-sm tracking-wider text-gray-500 hover:text-gray-900 dark:hover:text-white transition-colors"
-                        >
-                            Send
-                        </button>
+                    <div className="pt-4 absolute pb-0 w-3/4 bottom-0">
+                                    <div className="write bg-white shadow flex rounded-lg">
+                                        <div className="flex-3 flex content-center items-center text-center p-4 pr-0">
+                                            <span className="block text-center text-gray-400 hover:text-gray-800">
+                                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" className="h-6 w-6"><path d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                            </span>
+                                        </div>
+                                        <div className="flex-1">
+                                            <textarea onChange={handleMessage} name="message" className="w-full block outline-none py-4 px-4 bg-transparent" placeholder="Type a message..." autoFocus></textarea>
+                                        </div>
+                                        <div className="flex-2 w-32 p-2 flex content-center items-center">
+                                            <div className="flex-1 text-center">
+                                                <span className="text-gray-400 hover:text-gray-800">
+                                                    <span className="inline-block align-text-bottom">
+                                                        <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" className="w-6 h-6"><path d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path></svg>
+                                                    </span>
+                                                </span>
+                                            </div>
+                                            <div className="flex-1">
+                                                <button className="bg-blue-400 w-10 h-10 rounded-full inline-block">
+                                                    <span className="inline-block align-text-bottom">
+                                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" className="w-4 h-4 text-white"><path d="M5 13l4 4L19 7"></path></svg>
+                                                    </span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                     </form>
                 </div>
             </div>
-        </>
+
+           
     )
 }
