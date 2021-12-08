9acf5bb69f38ac8f984e141f6c2807a47e2120de
Anton Munter <am223yd@student.lnu.se>
2021-12-08 11:41:06 +0100

    Remove secrets

--- infrastructure/terraform/security-groups.tf
+++ infrastructure/terraform/security-groups.tf
@@ -7,8 +7,8 @@ resource "openstack_networking_secgroup_rule_v2" "db_secgroup_rule_1" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 3306
-  port_range_max    = 3306
+  port_range_min    = 5432
+  port_range_max    = 5432
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.db_secgroup.id
 }
@@ -17,8 +17,8 @@ resource "openstack_networking_secgroup_rule_v2" "db_secgroup_rule_2" {
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 3306
-  port_range_max    = 3306
+  port_range_min    = 5432
+  port_range_max    = 5432
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.db_secgroup.id
 }
