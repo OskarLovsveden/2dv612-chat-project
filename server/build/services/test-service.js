aa073d3cbef30e041cb9f1ad270d951329066599
Anton Munter <am223yd@student.lnu.se>
2021-12-15 14:38:08 +0100

    Remove build files

--- server/build/services/test-service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TestService {
-    sayHello() {
-        return 'Hello Lars';
-    }
-}
-exports.default = TestService;
