ed1f06edabfc9829a6361431338bb237e6ed5108
Anton Munter <am223yd@student.lnu.se>
2021-12-21 14:37:40 +0100

    Change rules for build

--- postgres/db.sql
+++ postgres/db.sql
@@ -17,7 +17,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: chatapp; Type: DATABASE; Schema: -; Owner: postgres
+-- Name: chatapp; Type: DATABASE; Schema: -; Owner: postgres 
 --
 
 ALTER DATABASE chatapp OWNER TO postgres;
