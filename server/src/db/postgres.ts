dbecab3636fea05d05aba119b2b63b6e409c16ff
Anton Munter <am223yd@student.lnu.se>
2021-12-23 17:55:53 +0100

    log env

--- server/src/db/postgres.ts
+++ server/src/db/postgres.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize';
 import * as pg from 'pg';
 
-const database = process.env.POSTGRES_DB || 'postgres';
+const database = process.env.POSTGRES_DB || 'chatapp';
 const host = process.env.POSTGRES_HOST || 'pg-svc.development.svc.cluster.local';
-const password = process.env.POSTGRES_PASSWORD || 'chatapp';
+const password = process.env.POSTGRES_PASSWORD;
 const username = process.env.POSTGRES_USER || 'postgres';
 const port = parseInt(process.env.POSTGRES_PORT || '5432');
 
