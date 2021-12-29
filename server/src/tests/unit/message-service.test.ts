7689c1315fabf6470d2a1fb62a4b03f0cb7434bd
Victor Ionzon <vi222bk@student.lnu.se>
2021-12-29 14:58:47 +0100

    Add creation test for services

--- server/src/tests/unit/message-service.test.ts
+++ server/src/tests/unit/message-service.test.ts
@@ -1,4 +1,4 @@
-// // Unit test the chatroom service.
+// // Unit test the message service.
 import { expect } from 'chai';
 import mockMessage from './models/messageMock';
 import MessageService from '../../services/message-service';
@@ -7,6 +7,12 @@ import Message from '../../models/message';
 const sut = new MessageService(mockMessage);
 const userID = 1;
 const msgID = 1;
+const newMsg = {
+    name: 'a new message',
+    message: 'testing new message',
+    user_id: 1,
+    room_id: 1
+};
 
 describe('Message service', () => {
     it('Should return message', async () => {
@@ -14,7 +20,7 @@ describe('Message service', () => {
 
         expect(actual)
             .to.be.an('object')
-            .to.have.deep.property('name')
+            .to.have.keys(['name', 'message', 'room_id', 'updatedAt', 'user_id', 'id', 'createdAt'])
             .not.to.be.undefined;
     });
 
@@ -33,5 +39,14 @@ describe('Message service', () => {
 
         const actual = await sut.getAll(userID);
         expect(actual).to.be.an('array').to.be.lengthOf(0);
+    });
+
+    it('Should create new message', async () => {
+        const message = await (await sut.create(newMsg)).toJSON();
+        
+        expect(message)
+            .to.be.an('object')
+            .to.have.keys(['name', 'message', 'room_id', 'updatedAt', 'user_id', 'id', 'createdAt'])
+            .not.to.be.undefined;
     });
 });
