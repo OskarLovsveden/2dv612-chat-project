a8fa0d0f5f44e22d3374c0ea89e578ce0757b5a5
Simon Palm <sp222ym@student.lnu.se>
2021-12-31 10:46:29 +0100

    Creating a landing view.

---
+++ client/src/components/Chatroom/NoChat.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NoChat:React.FC = () => {
+    return (
+        <div className="max-w-auto h-screen w-full m-auto bg-indigo-300 rounded p-5">
+        </div>
+    )
+}
+
+export default NoChat
