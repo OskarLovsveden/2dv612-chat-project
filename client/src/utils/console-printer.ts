c96de19e8b65f05935e574d77a5df92bd3dafaa5
Stefan Jagstrand <sj223gg@student.lnu.se>
2022-01-05 15:12:41 +0100

    Fix lint

--- client/src/utils/console-printer.ts
+++ client/src/utils/console-printer.ts
@@ -2,8 +2,8 @@
  * Only prints to console if NODE_ENV is not production!
  * Handy for debug console.logs!
  */
-export const printToConsole = (message: string) => {
-    if (process.env.NODE_ENV != 'production') {
+export const printToConsole = (message: string): void => {
+    if (process.env.NODE_ENV !== 'production') {
         console.log(message);
     }
 };
--- server/src/utils/console-printer.ts
+++ server/src/utils/console-printer.ts
@@ -1,9 +1,9 @@
 /**
- * Only prints to console if NODE_ENV=development!
+ * Only prints to console if NODE_ENV is not production!
  * Handy for debug console.logs!
  */
-export const printToConsole = (message: string) => {
-    if (process.env.NODE_ENV != 'production') {
+export const printToConsole = (message: string): void => {
+    if (process.env.NODE_ENV !== 'production') {
         console.log(message);
     }
 };
