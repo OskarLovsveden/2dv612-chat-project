d4f4ca285328c2982217b39f72c1c101c714cdc3
ol222hf <ol222hf@student.lnu.se>
2022-01-05 16:36:51 +0100

    update test

--- server/src/tests/unit/message-service.test.ts
+++ server/src/tests/unit/message-service.test.ts
@@ -2,10 +2,10 @@
 import { expect } from 'chai';
 import mockMessage from './models/messageMock';
 import MessageService from '../../services/message-service';
-import Message from '../../models/message';
+// import Message from '../../models/message';
 
 const sut = new MessageService(mockMessage);
-const userID = 1;
+// const userID = 1;
 const msgID = 1;
 const newMsg = {
     name: 'a new message',
@@ -24,22 +24,24 @@ describe('Message service', () => {
             .not.to.be.undefined;
     });
 
-    it('Should return array of messages', async () => {
-        const actual = await (await sut.getAll(userID)).map((a: Message) => a.toJSON());
+    // METHOD NO LONGER EXIST YO
+    // it('Should return array of messages', async () => {
+    //     const actual = await (await sut.getAll(userID)).map((a: Message) => a.toJSON());
         
-        expect(actual).to.be.lengthOf.greaterThan(0).to.be.an('array');
-    });
+    //     expect(actual).to.be.lengthOf.greaterThan(0).to.be.an('array');
+    // });
 
-    it('Should return empty message array', async () => {
-        mockMessage.$queryInterface.$useHandler((query: string) => {
-            if (query === 'findAll') {
-                return [];
-            }
-        });
+    // METHOD NO LONGER EXIST EITHER YO (:
+    // it('Should return empty message array', async () => {
+    //     mockMessage.$queryInterface.$useHandler((query: string) => {
+    //         if (query === 'findAll') {
+    //             return [];
+    //         }
+    //     });
 
-        const actual = await sut.getAll(userID);
-        expect(actual).to.be.an('array').to.be.lengthOf(0);
-    });
+    //     const actual = await sut.getAll(userID);
+    //     expect(actual).to.be.an('array').to.be.lengthOf(0);
+    // });
 
     it('Should create new message', async () => {
         const message = await (await sut.create(newMsg)).toJSON();
