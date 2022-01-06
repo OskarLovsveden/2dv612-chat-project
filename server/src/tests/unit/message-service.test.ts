7f48f37c54f880b9e9886113c719d1f99ed5cc4e
Victor Ionzon <vi222bk@student.lnu.se>
2022-01-06 09:45:45 +0100

    Removing incorrect tests in message-service

--- server/src/tests/unit/message-service.test.ts
+++ server/src/tests/unit/message-service.test.ts
@@ -24,25 +24,6 @@ describe('Message service', () => {
             .not.to.be.undefined;
     });
 
-    // METHOD NO LONGER EXIST YO
-    // it('Should return array of messages', async () => {
-    //     const actual = await (await sut.getAll(userID)).map((a: Message) => a.toJSON());
-        
-    //     expect(actual).to.be.lengthOf.greaterThan(0).to.be.an('array');
-    // });
-
-    // METHOD NO LONGER EXIST EITHER YO (:
-    // it('Should return empty message array', async () => {
-    //     mockMessage.$queryInterface.$useHandler((query: string) => {
-    //         if (query === 'findAll') {
-    //             return [];
-    //         }
-    //     });
-
-    //     const actual = await sut.getAll(userID);
-    //     expect(actual).to.be.an('array').to.be.lengthOf(0);
-    // });
-
     it('Should create new message', async () => {
         const message = await (await sut.create(newMsg)).toJSON();
         
